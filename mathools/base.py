a46889ca9ce2197fcb570d46def7f9e2c13349ef
Julius Rye Bønnelykke <juliusrye@gmail.com>
2022-07-13 17:04:32 +0200

    Added more functionality to base.py

--- mathools/base.py
+++ mathools/base.py
@@ -12,6 +12,10 @@ class Base():
             symbols = np.array(sym, dtype=str)
         self.symbols = symbols
         
+        self.symbols_dic = {}
+        for i, s in enumerate(self.symbols):
+            self.symbols_dic[s] = i
+        
         if self.base > len(self.symbols):
             raise ValueError(f"Not enough symbols (number of symbols: {len(self.symbols)}) for the base {self.base}")
     
@@ -27,10 +31,26 @@ class Base():
         
         return digits, orders
     
-    def convert_value(self, v: float) -> str:
+    def value_to_base(self, v: float) -> str:
         digits, orders = self._factor(v)
         return "".join([self.symbols[digits[i]] for i in orders])
+    
+    def value_from_base(self, v: str) -> float:
+        num = 0
+        for i, letter in enumerate(v):
+            num += self.base**(len(v)-i-1)*self.symbols_dic[letter]
+        return num
+    
+    def convert_base(self, v: str, to_base) -> str:
+        num = self.value_from_base(v)
+        return to_base.value_to_base(num)
 
 if __name__ == "__main__":
     b16 = Base(16)
-    print(b16.convert_value(128))
+    txt1 = b16.value_to_base(7842)
+    print(txt1)
+    b2 = Base(2)
+    txt2 = b16.convert_base(txt1, b2)
+    print(txt2)
+    val = b2.value_from_base(txt2)
+    print(val)
